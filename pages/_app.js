ce2404fdc3b4be09198a202b7eb77899712ef6ff
fturello <92311869+fturello@users.noreply.github.com>
2023-02-24 13:06:07 +0100

    Global styles added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import { Provider as StyletronProvider } from 'styletron-react'
-import { styletron } from '../styletron'
+import { Provider as StyletronProvider } from "styletron-react";
+import { styletron } from "../styletron";
+
+import "../index.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <StyletronProvider value={styletron}>
-      <Component {...pageProps} />
-    </StyletronProvider>
-  )
+	return (
+		<StyletronProvider value={styletron}>
+			<Component {...pageProps} />
+		</StyletronProvider>
+	);
 }
